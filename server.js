834de3e10da4c8b4097746adec29f8d1f1773f85
TDB <attila.kisivan@gmail.com>
2023-01-29 09:58:57 +0000

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'dpg-cfaf3ohgp3jsh6f1lo2g-a',
-    user : 'face_recognition_app_backend_database_user',
-    password : '',
-    database : 'face_recognition_app_backend_database'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
