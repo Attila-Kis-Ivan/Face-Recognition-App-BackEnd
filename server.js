89d67f46a0474448d40887dc8349bd38ee396c7a
TDB <attila.kisivan@gmail.com>
2023-01-29 17:06:05 +0000

    still trying add .env

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_HOST,
-    user : process.env.DATABASE_USER,
+    host : 'dpg-cfaf3ohgp3jsh6f1lo2g-a',
+    user : 'face_recognition_app_backend_database_user',
     password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_DATABASE
+    database : 'face_recognition_app_backend_database'
   }
 });
 
